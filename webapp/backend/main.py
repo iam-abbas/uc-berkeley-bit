81214d96a4c8a98186d80f5081b1a28517e7ba05
iam-abbas <iamabbas7@gmail.com>
2020-04-19 15:49:37 -0700

    fixed progress bar and code clean

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, jsonify
 from flask_mysqldb import MySQL
 
-app = Flask(__name__,  template_folder='../frontend', static_folder="../frontend/assets/")
+app = Flask(__name__,  template_folder='../frontend',
+            static_folder="../frontend/assets/")
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -13,7 +14,7 @@ mysql = MySQL(app)
 
 
 @app.route('/post', methods=['POST'])
-def hello_world():
+def post_data():
     if request.method == "POST":
         cur = mysql.connection.cursor()
         data = request.get_json()
@@ -24,11 +25,11 @@ def hello_world():
         for i in range(1, 39):
             stmt += f"'{data[0]['Q'+str(i)+'-Ans']}', '{data[1]['Q'+str(i)+'-Time']}', "
         stmt += f"'{data[0]['Demo1']}', '{data[0]['Demo2']}', '{data[0]['Demo3']}', '{data[0]['Demo4']}')"
-        print(stmt)
         cur.execute(stmt)
         mysql.connection.commit()
         cur.close()
-    return jsonify({"success":True})
+    return jsonify({"success": True})
+
 
 @app.route('/survey')
 def survey():

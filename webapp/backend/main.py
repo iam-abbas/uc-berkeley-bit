28ab4d9d25fae742a67445655d0677ba4bfc1553
iam-abbas <iamabbas7@gmail.com>
2020-04-19 19:01:56 -0700

    updated static urls

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, jsonify
 from flask_mysqldb import MySQL
 
-app = Flask(__name__,  template_folder='../frontend')
+app = Flask(__name__,  template_folder='../frontend',
+            static_folder="../frontend/assets/")
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'

8c0318662948bee1b8f5c1f60c1c9c5573fe57e3
joisse1101 <thoiwei@gmail.com>
2020-04-23 14:35:38 +0800

    Add simple email verification
    Error message now pops up when user clicks away from email input box. Prevents user from moving on to next question without first correctly inputting email.

--- webapp/frontend/assets/js/jquery.js
+++ webapp/frontend/assets/js/jquery.js
@@ -1,6 +1,7 @@
 var startTime;
 var lapsetime = 0;
 var timeobj = {};
+
 function taptime(qdata) {
   if (startTime) {
     lapsetime = new Date().getTime() - startTime - lapsetime;
@@ -35,3 +36,15 @@ $(".lastone").click(function () {
     },
   });
 });
+
+function validateEmail(sEmail) {
+  var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+
+  if(!sEmail.match(reEmail)) {
+    alert("Invalid email address");
+    return false;
+  }
+
+  return true;
+
+}

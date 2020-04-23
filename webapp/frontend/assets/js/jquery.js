77018efe6dd0625ebe78c7c79df5d146371a6860
joisse1101 <thoiwei@gmail.com>
2020-04-23 18:50:53 +0800

    Change email input alert to include -No email entered- message

--- webapp/frontend/assets/js/jquery.js
+++ webapp/frontend/assets/js/jquery.js
@@ -41,10 +41,13 @@ function validateEmail(sEmail) {
   var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
 
   if(!sEmail.match(reEmail)) {
-    alert("Invalid email address");
+    if (sEmail.length == 0) {
+        alert("No email entered");
+    }
+    else {
+        alert("Invalid email address");
+    }
     return false;
   }
-
   return true;
-
 }
